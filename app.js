c1c6ba483452ac29e6aded43af158f359cb4bfdd
nimisha27 <112835322+nimisha-527@users.noreply.github.com>
2025-02-02 18:10:55 +0530

    Worked on adding mongo sanitize

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const User = require('./models/user')
+const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 mongoose.connect('mongodb://localhost:27017/reading-bliss', {
     useNewURLParser: true,
@@ -45,6 +46,11 @@ app.set('view engine', 'ejs');
 app.use(methodOverride("_method"));
 app.use(express.static( path.join(__dirname, "public") ));
 app.use(express.urlencoded({extended: true}));
+app.use(
+    mongoSanitize({
+      replaceWith: '--',
+    }),
+  );
 
 const sessionConfig = {
     secret: "thisisnotasecret",
