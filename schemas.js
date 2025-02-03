a8d2ea3c1b4d61ca639cae20ed281621f1674122
nimisha27 <112835322+nimisha-527@users.noreply.github.com>
2025-02-03 18:38:00 +0530

    basic security fixes

--- schemas.js
+++ schemas.js
@@ -1,15 +1,39 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+// const { type } = require('os');
+const sanitize = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitize(value, {
+                    allowedTages: [],
+                    allowedAttributes: {}
+                });
+                if(clean !== value) return helpers.error('string.escapeHTML', {value})
+                    return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
 
 module.exports.booksSchema = Joi.object({
-    title: Joi.string().required(),
-    author: Joi.string().required(),
-    description: Joi.string().required(),
-    category: Joi.string().required()
+    title: Joi.string().required().escapeHTML(),
+    author: Joi.string().required().escapeHTML(),
+    description: Joi.string().required().escapeHTML(),
+    category: Joi.string().required().escapeHTML()
 }).required();
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
-        body: Joi.string().required(),
+        body: Joi.string().required().escapeHTML(),
         rating: Joi.number().required() 
     }).required()
 })
