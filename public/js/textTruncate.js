2f4287b7df75bf7f1f2e98ab177f01c1380b0984
nimisha27 <112835322+nimisha-527@users.noreply.github.com>
2025-03-21 08:02:58 +0530

    removed commented out code

--- public/js/textTruncate.js
+++ public/js/textTruncate.js
@@ -1,35 +1,3 @@
-// READ MORE
-// const textContent = document.getElementById("text-truncate-content");
-// const readMoreBtn = document.getElementById("read-more-btn");
-// const dataBsWhatever = textContent.getAttribute("data-bs-whatever");
-
-// let isExpanded = false;
-
-// readMoreBtn.style.cursor = "pointer";
-
-// function truncateText() {
-//   const words = textContent.textContent.split(" ");
-//   const truncatedWords = words.slice(0, 30);
-//   textContent.textContent = truncatedWords.join(" ") + "...";
-// }
-
-// function toggleText() {
-//   if (!isExpanded) {
-//     textContent.textContent = dataBsWhatever;
-//     readMoreBtn.textContent = "Read Less";
-//   } else {
-//     truncateText();
-//     readMoreBtn.textContent = "Read More";
-//   }
-//   isExpanded = !isExpanded;
-// }
-
-// readMoreBtn.addEventListener("click", toggleText);
-
-// // Initial truncation
-// truncateText();
-
-
 const textContent = document.querySelectorAll(".text-truncate-content");
 const readMoreBtn = document.getElementById("read-more-btn");
 
